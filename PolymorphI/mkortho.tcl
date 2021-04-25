cec997fb0e4f0ec12050ee706126a41ae8723b25
Cameron Abrams <cfa22@drexel.edu>
2021-04-25 13:23:40 -0400

    1

--- PolymorphI/mkortho.tcl
+++ PolymorphI/mkortho.tcl
@@ -8,12 +8,20 @@ set p [lindex [pbc get] 0]
 set a [lindex $p 0]
 set b [lindex $p 1]
 set c [lindex $p 2]
-set alpha [lindex $p 3]
-set beta [lindex $p 4]
-set gamma [lindex $p 5]
-set o_a [expr $a*sin($gamma*3.141593/180)]
-set o_c [expr $c*sin($beta*3.141593/180)*sin($alpha*3.141593/180)]
+set d2r [expr 3.141593/180.0]
+set alpha [expr [lindex $p 3]*$d2r]
+set beta [expr [lindex $p 4]*$d2r]
+set gamma [expr [lindex $p 5]*$d2r]
+set o_a $a
+set xy [expr $b*cos($gamma)]
+set xz [expr $c*cos($beta)]
+set ly [expr sqrt($b$b-$xy*$xy)]
+set yz [expr ($b*$c*cos($alpha)-$xy*$xz)/$ly]
+set lz [expr sqrt($c*$c-$xz$*$xz-$yz*$yz)]
+set o_b $ly
+set o_c $lz
 lset p 0 $o_a
+lset p 1 $o_b
 lset p 2 $o_c
 lset p 3 90.000
 lset p 4 90.000
