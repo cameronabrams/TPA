cff289e98cd2b2b7070fdfed45af93e7cffd8c13
Cameron Abrams <cfa22@drexel.edu>
2021-04-26 08:51:38 -0400

    new

---
+++ PolymorphIII/mkcrystpsf.tcl
@@ -0,0 +1,31 @@
+package require pbctools
+mol new unitcell_mol1.pdb
+set p [pbc get]
+
+package require psfgen
+topology /home/cfa/charmm/toppar/top_all36_prot.rtf
+topology /home/cfa/charmm/toppar/top_all36_carb.rtf
+topology /home/cfa/charmm/toppar/top_all36_lipid.rtf
+topology /home/cfa/charmm/toppar/top_all36_na.rtf
+topology /home/cfa/charmm/toppar/top_all36_cgenff.rtf
+topology ./ub7.str
+
+segment A {
+    pdb unitcell_mol1.pdb
+}
+segment B {
+    pdb unitcell_mol2.pdb
+}
+
+coordpdb unitcell_mol1.pdb A
+coordpdb unitcell_mol2.pdb B
+
+guesscoord
+
+writepsf my_ub7_unitcell.psf
+writepdb my_ub7_unitcell_tmp.pdb
+
+mol new my_ub7_unitcell_tmp.pdb
+pbc set $p
+[atomselect top all] writepdb my_ub7_unitcell.pdb
+exit
